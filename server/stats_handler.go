5d3c540d20cef86575d0f7320983dc621a72e2c9
Alex Raskin <alexraskin@fastmail.fm>
2025-05-07 18:35:49 -0700

    update stats endpoint

--- server/stats_handler.go
+++ server/stats_handler.go
@@ -2,7 +2,9 @@ package server
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"runtime"
 )
 
 type statsHandler struct {
@@ -24,8 +26,10 @@ func (h *statsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 type statsResponse struct {
-	Success       bool `json:"success"`
-	ResourceCount int  `json:"resourceCount"`
+	Success       bool   `json:"success"`
+	TinyGoVersion string `json:"tinygoVersion"`
+	MemoryUsed    string `json:"memoryUsed"`
+	ResourceCount int    `json:"resourceCount"`
 }
 
 func (h *statsHandler) stats(w http.ResponseWriter, r *http.Request) {
@@ -40,9 +44,26 @@ func (h *statsHandler) stats(w http.ResponseWriter, r *http.Request) {
 		h.server.handleError(w, "Internal server error", http.StatusInternalServerError, err.Error())
 		return
 	}
+	stats := runtime.MemStats{}
+	runtime.ReadMemStats(&stats)
 
 	json.NewEncoder(w).Encode(statsResponse{
 		Success:       true,
+		TinyGoVersion: runtime.Version(),
+		MemoryUsed:    fmt.Sprintf("%s / %s (%s garbage collected)", humanizeBytes(stats.Alloc), humanizeBytes(stats.Sys), humanizeBytes(stats.TotalAlloc)),
 		ResourceCount: len(objects.Objects),
 	})
+}
+
+func humanizeBytes(bytes uint64) string {
+	switch {
+	case bytes >= 1<<30:
+		return fmt.Sprintf("%.2f GB", float64(bytes)/(1<<30))
+	case bytes >= 1<<20:
+		return fmt.Sprintf("%.2f MB", float64(bytes)/(1<<20))
+	case bytes >= 1<<10:
+		return fmt.Sprintf("%.2f KB", float64(bytes)/(1<<10))
+	default:
+		return fmt.Sprintf("%d B", bytes)
+	}
 }
